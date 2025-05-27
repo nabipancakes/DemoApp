4d74fc0044ee21746a3354cb3e8fead72efb9a05
Benjamin Guo <bguo12345678@gmail.com>
2025-05-27 13:34:41 -0500

    deleted xoxo

--- DemoApp/ContentView.swift
+++ DemoApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Bye, world! xoxo stephanie.")
+            Text("Bye, world!")
         }
         .padding()
     }
