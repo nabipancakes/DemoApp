8dbf558aaf300bd00b9d65207c5a19fc06884db9
nabipancakes <nabipancakes@gmail.com>
2025-05-27 12:49:29 -0500

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Stephanie Shen on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoAppTests/DemoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppTests.swift
+//  DemoAppTests
+//
+//  Created by Stephanie Shen on 5/27/25.
+//
+
+import Testing
+@testable import DemoApp
+
+struct DemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
